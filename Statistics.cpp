85974d9e4ed6e5947d7fbfd5a17cdd17761d3e82
James Petullo <pentester18@gmail.com>
2016-11-25 11:56:47 -0500

    Add files via upload
    This is an updated version of my original statistics program. In this program, the user enters a list of all the numbers to a file and opens it in the program, which in turn will calculate the min, max, mean, range, and standard deviation.

--- Statistics.cpp
+++ Statistics.cpp
@@ -1,38 +1,52 @@
 //This program calculates the mean, min, max, and standard deviation of a set of data
 #include <iostream>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-  int N, set = 0, max = 0, maximum, min = 10, minimum, range;
+  ifstream inputfile;
+  int set = 0, max = 0, maximum, min = 10, minimum, range, count = 0, counter = 0, number, numberofnumbers;
   double average, ssdev = 0;
   double standard_deviation;
-  cout << "Enter your number of numbers: ";
-  cin >> N;
-  int setofnumbers[N]; //this loop allows users to enter their list of data
-  for (int i = 0; i < N-1; i++)
-  {
-    cout << "Enter your number: ";
-    cin >> setofnumbers[i];
+  cout << "Please enter the number of numbers in your data file: "<< endl;
+  cin >> numberofnumbers;
 
+  if (inputfile.fail())
+  {
+    cout << "Failure to open";
+    exit(1);
   }
 
-  for (int i = 0; i < N-1; i++) //this loop calculates the average
+  inputfile.open("/Users/davidjpetullo/Documents/Final_Output.txt");
+
+
+  int setofnumbers[numberofnumbers];
+  while(!inputfile.eof())
+  {
+    inputfile >> setofnumbers[count];
+    count++;
+  }
+  count = count - 1;
+
+  for (int i = 0; i < count; i++) //this loop calculates the average
   {
     set = setofnumbers[i] + set;
 
+
   }
-  average = (double)set/(double)N; //average calculator
+
+  average = (double)set/(double)count; //average calculator
 
 
-  for (int i = 0; i < N-1; i++) //this loop calculates the standard deviation
+  for (int i = 0; i < count-1; i++) //this loop calculates the standard deviation
   {
     ssdev = pow(((double)setofnumbers[i]-average), 2.0) + ssdev;
   }
-  standard_deviation = pow((ssdev/((double)N-1)), 0.50);
+  standard_deviation = pow((ssdev/((double)count-1)), 0.50);
 
-  for (int i = 0; i < N-1; i++) //this loop finds the maximum number of the data set
+  for (int i = 0; i < count-1; i++) //this loop finds the maximum number of the data set
   {
     if (setofnumbers[i] > max)
     {
@@ -40,25 +54,25 @@ int main()
 
     }
   }
-    maximum = max;
+  maximum = max;
 
-    for (int i = 0; i < N - 1; i++) //this loop finds the minimum number in the data set
+  for (int i = 0; i < count - 1; i++) //this loop finds the minimum number in the data set
+  {
+    if (setofnumbers[i] < min)
     {
-      if (setofnumbers[i] < min)
-      {
-        min = setofnumbers[i];
-
-      }
+      min = setofnumbers[i];
 
     }
-    minimum = min;
 
-    range = maximum - minimum;
+  }
+  minimum = min;
 
-    cout << "Mean: "<< average << endl;
-    cout << "Max: "<< maximum << endl;
-    cout << "Min: "<< minimum << endl;
-    cout << "Range: "<< range << endl;
-    cout << "Standard Deviation: "<< standard_deviation << endl;
+  range = maximum - minimum;
+
+  cout << "Mean: "<< average << endl;
+  cout << "Max: "<< maximum << endl;
+  cout << "Min: "<< minimum << endl;
+  cout << "Range: "<< range << endl;
+  cout << "Standard Deviation: "<< standard_deviation << endl;
 
 }
